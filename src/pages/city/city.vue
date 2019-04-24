474fc3ccf62fe1fdd79b89f8f45d09d9eacffb28
123 <xfc@123deMacBook-Pro.local>
2019-04-24 13:43:37 +0800

    search

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
     <city-header></city-header>
+    <city-search></city-search>
   </div>
 </template>
 
 <script>
 import CityHeader from './component/header'
+import CitySearch from './component/search'
 export default {
   name: 'city',
-  components: {CityHeader}
+  components: {CitySearch, CityHeader}
 }
 </script>
 
