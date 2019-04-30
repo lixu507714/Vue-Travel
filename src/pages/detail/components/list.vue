9c2b021cc1e96d4940e69d2be93b3ea9a0a68268
123 <xfc@123deMacBook-Pro.local>
2019-04-30 10:35:10 +0800

    递归

---
+++ src/pages/detail/components/list.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="item">
+    <div class="item-title border-bottom" v-for="item of list" :key="item.name">
+      <span class="item-title-icon">
+        {{item.title}}
+      </span>
+      <div v-show="item.children" class="item-title-child">
+        <detail-list :list="item.children"></detail-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props: {
+    list: Array
+  },
+  methods: {
+
+  }
+}
+</script>
+<style lang="stylus" scoped>
+@import "../../../assets/styles/main.styl";
+.item-title {
+  line-height:0.8rem;
+  font-size:0.32rem;
+  padding:0 0.2rem;
+}
+.item-title-child {
+  padding:0 0.2rem;
+}
+</style>
--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -2,16 +2,60 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/banner'
 import DetailHeader from './components/header'
+import DetailList from './components/list'
 export default {
   name: 'Detail',
-  components: {DetailHeader, DetailBanner}
+  components: {DetailList, DetailHeader, DetailBanner},
+  data () {
+    return {
+      list: [
+        {
+          title: '成人票',
+          children: [
+            {
+              title: '成人票 - 五一活动'
+            },
+            {
+              title: '成人票 - 五一活动'
+            },
+            {
+              title: '成人票 - 五一活动'
+            }
+          ]
+        }, {
+          title: '儿童票',
+          children: [
+            {
+              title: '儿童票 - 五一活动'
+            }
+          ]
+        }, {
+          title: '优惠票',
+          children: [
+            {
+              title: '儿童票 - 五一活动'
+            }
+          ]
+        }, {
+          title: '团票',
+          children: [
+            {
+              title: '儿童票 - 五一活动'
+            }
+          ]
+        }
+      ]
+    }
+  }
 }
 </script>
 
